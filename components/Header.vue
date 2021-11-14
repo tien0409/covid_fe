0d56db896cff4eb5c5ea65aac9400b6c27e137ca
Le Anh Tien <tienla@flextech.vn>
2021-11-14 19:51:49 +0700

    feat: add Header component

---
+++ components/Header.vue
@@ -0,0 +1,10 @@
+<template>
+    <el-menu mode="horizontal" router>
+        <el-menu-item index="/home">
+            Khai báo
+        </el-menu-item>
+        <el-menu-item index="/information">
+            Thông tin dịch bệnh
+        </el-menu-item>
+    </el-menu>
+</template>
