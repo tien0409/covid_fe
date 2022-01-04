037c0ea72c8b4530dc67ec3ff5e54944b9e0d09f
Le Anh Tien <tienla@flextech.vn>
2022-01-04 14:49:53 +0700

    fix: format eslint

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,11 +8,11 @@
 </template>
 
 <script>
-    import Header from "@/components/Header.vue"
+    import Header from '@/components/Header.vue';
 
     export default {
         components: {
-            Header
-        }
-    }
+            Header,
+        },
+    };
 </script>
